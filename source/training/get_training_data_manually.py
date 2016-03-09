97da83920f4c5de2fac5d74c50ae877d6972adae
jimchen <jimchen0605@gmail.com>
2016-03-09 14:56:53 +0800

    修改手动修改的代码

--- source/training/get_training_data_manually.py
+++ source/training/get_training_data_manually.py
@@ -1,8 +1,8 @@
 '''
-Get Training Data without screen
+Get Training Data Manually
 ~~~~~~~~
 
-Similar like get_training_data.py but use raw_input to instead of cv2.waitKey, which do not use imshow as well.
+something wrong with raspberry pi
 '''
 
 ## Library
@@ -16,24 +16,21 @@ import numpy as np
 import cv2
 
 # my library - configuration
-sys.path.append("/home/pi/Documents/Github/Smart-Car/source/config")
-sys.path.append("/home/pi/Documents/Github/Smart-Car/source/component")
-import move
+sys.path.append("../config")
+sys.path.append("../component")
 import image_preprocess as imgprocess
-import common as common_config
 
 # save image function
 def save_image(direction, image):
     now = int(time.time())
-    name = "/home/pi/Documents/Github/Smart-Car/source/data/"+str(now)+"-"+str(direction)+"-"+str(random.randint(1, 1000))+".jpg"
+    name = "../data/"+str(now)+"-"+str(direction)+"-"+str(random.randint(1, 1000))+".jpg"
     print "want to save file:"+name
     cv2.imwrite(name, image)
 
 
 ## Main function
 # major variables
-cap = cv2.VideoCapture(0)
-mo = move.Move()
+cap = cv2.VideoCapture(1)
 
 while(cap.isOpened()):
     # Capture frame-by-frame
@@ -42,38 +39,25 @@ while(cap.isOpened()):
     # Our operations on the frame come here
     # Change frame to gray level image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img = imgprocess.imageDW(gray,gray.shape,4)
+    img = imgprocess.imageDW(gray,gray.shape,2)
 
     # Do not display the resulting frame
-    #cv2.imshow('frame',frame)
+    cv2.imshow('frame',img)
 
     # Choose direction or quit
-    input_key = raw_input("where to go (h for hint):")
-    if input_key == "q":
+    input_key = cv2.waitKey(1) & 0xFF
+    if input_key == ord('q'):
         break
-    elif input_key == "w":
-        mo.forward(common_config.SLEEP_TIME)
-        save_image("w", gray)
-    elif input_key == "s":
-        mo.backward(common_config.SLEEP_TIME)
-        save_image("s", gray)
-    elif input_key == "a":
-        mo.turn_left(common_config.SLEEP_TIME)
-        save_image("a", gray)
-    elif input_key == "d":
-        mo.turn_right(common_config.SLEEP_TIME)
-        save_image("d", gray)
-    elif input_key == "h":
-        print "Instructions:\n"
-        print "w: ahead\ns: reverse\na: turn left\nd: turn right\nq: quit\n"
+    elif input_key == ord('w'):
+        save_image("w", img)
+    elif input_key == ord('s'):
+        save_image("s", img)
+    elif input_key == ord('a'):
+        save_image("a", img)
+    elif input_key == ord('d'):
+        save_image("d", img)
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
 print "Finish recording"
-
-# shutdown the car
-mo.stop()
-mo.shutdown()
-print "Car shutdown"
-
