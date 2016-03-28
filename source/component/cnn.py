87da9dcbb68b997aa510f0a4bcadac66f01493d3
jimchenhub <935432685@qq.com>
2016-03-28 16:42:35 +0800

    initialize the weights and bias

--- source/component/cnn.py
+++ source/component/cnn.py
@@ -46,7 +46,21 @@ from theano.tensor import tanh
 
 #### Load the MNIST data
 def load_data_shared(filename="../data/expanded/data.pkl"):
-    pass
+    f = open(DATA_DIR+"data.pkl", 'rb')
+    training_data, validation_data, test_data = cPickle.load(f)
+    f.close()
+    def shared(data):
+        '''
+        Place the data into shared variables.  This allows Theano to copy
+        the data to the GPU, if one is available.
+        
+        是用borrow属性让get_value的时候值更新的时候能够获取到 
+        more detail： http://deeplearning.net/software/theano/tutorial/aliasing.html#borrowing-when-creating-shared-variables
+        '''
+        shared_x = theano.shared(np.asarray(data[0], dtype=theano.config.floatX), borrow=True)
+        shared_y = theano.shared(np.asarray(data[1], dtype=theano.config.floatX), borrow=True)
+        return shared_x, T.cast(shared_y, "int32")
+    return [shared(training_data), shared(validation_data), shared(test_data)]
 
 #### Main class used to construct and train networks
 class Network(object):
@@ -56,13 +70,22 @@ class Network(object):
         a value for the `mini_batch_size` to be used during training
         by stochastic gradient descent.
         '''
-
         self.layers = layers
         self.mini_batch_size = mini_batch_size
+        self.params = [param for layer in self.layers for param in layer.params]
+        self.x = T.matrix("x") # input
+        self.y = T.ivector("y") # output
+        init_layer = self.layers[0]
+        init_layer.set_inpt(self.x, self.x, self.mini_batch_size)
+        for i in xrange(1, len(len(layers))):
+            prev_layer, layer = self.layers[i-1], self.layers[i]
+            layer.set_inpt(prev_layer.output, prev_layer.output_dropout, self.mini_batch_size)
+        self.output = self.layers[-1].output
+        self.output_dropout = self.layers[-1].output_dropout
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             validation_data, test_data, lmbda=0.0):
-        pass
+        
 
 #### Define layer types
 
@@ -75,7 +98,7 @@ class ConvPoolLayer(object):
     '''
 
     def __init__(self, filter_shape, image_shape, poolsize=(2, 2),
-                 activation_fn=sigmoid):
+                 activation_fn=ReLU):
         '''
         `filter_shape` is a tuple of length 4, whose entries are the number
         of filters, the number of input feature maps, the filter height, and the
@@ -93,7 +116,16 @@ class ConvPoolLayer(object):
         self.poolsize = poolsize
         self.activation_fn=activation_fn
         # initialize weights and biases
-        pass
+        n_out = filter_shape[0] * np.prod(filter_shape[2:]) / np.prod(poolsize)
+        self.w = theano.shared(
+            np.asarray(
+                np.random.normal(loc=0, scale=np.sqrt(1.0/n_out), size=filter_shape),
+                dtype=theano.config.floatX),borrow=True)
+        self.b = theano.shared(
+            np.asarray(
+                np.random.normal(loc=0, scale=1.0, size=(filter_shape[0],)),
+                dtype=theano.config.floatX),borrow=True)
+        self.params = [self.w, self.b]
 
     def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
         # no dropout in the convolutional layers
@@ -107,7 +139,17 @@ class FullyConnectedLayer(object):
         self.activation_fn = activation_fn
         self.p_dropout = p_dropout
         # Initialize weights and biases
-        pass
+        self.w = theano.shared(
+                np.asarray(
+                    np.random.normal(loc=0.0, scale=np.sqrt(1.0/n_out), size=(n_in, n_out)
+                ), dtype=theano.config.floatX),name="w", borrow=True
+            )
+        self.b = theano.shared(
+                np.asarray(
+                    np.random.normal(loc=0.0, scale=np.sqrt(1.0/n_out), size=(n_out)
+                ), dtype=theano.config.floatX),name="b", borrow=True
+            )
+        self.params=[self.w, self.b]
 
     def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
         pass
@@ -123,6 +165,14 @@ class SoftmaxLayer(object):
         self.n_in = n_in
         self.n_out = n_out
         self.p_dropout = p_dropout
+        # Initialize weights and biases
+        self.w = theano.shared(
+            np.zeros((n_in, n_out), dtype=theano.config.floatX),
+            name='w', borrow=True)
+        self.b = theano.shared(
+            np.zeros((n_out,), dtype=theano.config.floatX),
+            name='b', borrow=True)
+        self.params = [self.w, self.b]
 
     def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
         pass
@@ -135,7 +185,8 @@ class SoftmaxLayer(object):
         "Return the accuracy for the mini-batch."
         pass
 
-# other functions
+### other functions
+
 def dropout_layer(layer, p_dropout):
     srng = shared_randomstreams.RandomStreams(
         np.random.RandomState(0).randint(999999))
