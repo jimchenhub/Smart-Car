4e4ceb02c43738ac2f1ad90e53d0e2abf2331b8f
lhx1992 <707564046@qq.com>
2016-04-26 20:38:31 +0800

    避障部分 (#17)
    双目标定：BinCalib.py、双目校正：rectify.py、运行：run.py。
    采集标定图片：camera_calibimg.py
    采集测试图片：camera_testimg.py

--- source/config/common.py
+++ source/config/common.py
@@ -12,3 +12,18 @@ CAP_HEIGHT = 48
 
 # network configuration
 NETWORK_INPUT_SIZE = 3072  # 64 * 48
+
+# Resolution ratio of binocular camera
+BINCAP_WIDTH = 640
+BINCAP_HEIGHT = 480
+
+# Real height(mm) of binocular camera
+BINCAP_REAL_HEIGHT = 150
+
+# Real Weight of car
+CAR_REAL_WIDTH = 130
+
+# Max distance(mm) of avoiding
+MAXDEPTH = 300
+
+
